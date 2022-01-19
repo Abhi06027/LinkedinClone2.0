6c1ce3f0e591991bb6e44db66a6de042d7dda9e1
Abhi06027 <abhinavk67@gmail.com>
2022-01-19 13:20:54 +0530

    sign up2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import Textfield from "./components/Textfield";
+import Headingtext from "./components/Headingtext";
+
+
+function App() {
+  return (
+    <>
+      <Headingtext heading="  Sign in  " />
+      <Headingtext heading="  Stay updated on your professional world  " />
+      <Textfield Name="Name " Password="  Password " Email=" Email "/>
+      
+      
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Headingtext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Headingtext(props) {
+  return (
+    <div>
+      <h1 className="  px-4 py-2  ">{props.heading}</h1>
+    </div>
+  );
+}
