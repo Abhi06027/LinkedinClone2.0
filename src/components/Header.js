480e04d83903781d4a956aa50141fb991f9fc66a
Abhi06027 <abhinavk67@gmail.com>
2022-01-25 15:05:55 +0530

    second file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header(props) {
   return (
-    <div className="  mt-72  pt-6  pl-8 max-w-lg mx-auto bg-white w-96  shadow-xl rounded-md   ">
+    <div className=" mt-20 lg:mt-60 pt-6  pl-8 max-w-lg mx-auto bg-white w-96  shadow-xl rounded-md   ">
       <h1 className="text-black  text-2xl font-bold   ">{props.name}</h1>
       <p className="text-black text-sm pb-5 pt-1 ">{props.name2}</p>
     </div>
--- src/components/Linkdinlogo.js
+++ src/components/Linkdinlogo.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Linkdinlogo(props) {
   return (
     <div className=" ">
-      <img className=" w-36  " src=".images/li.png " alt="" />
+      <img className="w-36 " src=".images/li.png" alt="" />
     </div>
   );
 }
