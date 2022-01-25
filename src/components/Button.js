9876e76f4af2aa1d3a589a193a82caaf9a672fab
Abhi06027 <abhinavk67@gmail.com>
2022-01-25 21:54:04 +0530

    second file

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,9 @@ export default function Button(props) {
   return (
     <div className="  flex justify-center  bg-white sm:shadow-xl  md:shadow-xl lg:shadow-xl  w-96 max-w-lg mx-auto cursor-pointer    ">
       <h1 className="bg-blue-700 rounded-full text-center w-80  hover:bg-blue-900 text-white py-3 mx-2">
-        {props.button}
+        <a href="https://www.linkedin.com/in/abhinav-gupta-03403a184/">
+          {props.button}
+        </a>
       </h1>
     </div>
   );
--- src/components/Linkdinlogo.js
+++ src/components/Linkdinlogo.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Linkdinlogo(props) {
-  return <div className=" "></div>;
+  return (
+    <div className=" ">
+      <a href="https://www.w3schools.com/">Visit W3Schools.com!</a>
+    </div>
+  );
 }
