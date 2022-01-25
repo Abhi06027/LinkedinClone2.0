4c49a1ad7c3437e1d302f2dba3eaf0afdd56b25f
Abhi06027 <abhinavk67@gmail.com>
2022-01-25 22:26:02 +0530

    second file

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Button(props) {
   return (
     <div className="  flex justify-center  bg-white sm:shadow-xl  md:shadow-xl lg:shadow-xl  w-96 max-w-lg mx-auto cursor-pointer    ">
-      <h1 className="bg-blue-700 rounded-full text-center w-80  hover:bg-blue-900 text-white py-3 mx-2">
-        <a href="https://www.linkedin.com/in/abhinav-gupta-03403a184/">
+      <a href="https://www.linkedin.com/in/abhinav-gupta-03403a184/">
+        <button className="bg-blue-700 rounded-full text-center w-80  hover:bg-blue-900 text-white py-3 mx-2">
           {props.button}
-        </a>
-      </h1>
+        </button>
+      </a>
     </div>
   );
 }
--- src/components/Buttonapple.js
+++ src/components/Buttonapple.js
@@ -3,10 +3,12 @@ import React from "react";
 export default function Buttonapple(props) {
   return (
     <div className=" flex justify-center  pt-2 pb-6  bg-white sm:shadow-xl  md:shadow-xl lg:shadow-xl w-96 max-w-lg mx-auto rounded-b-lg ">
-      <div className=" flex  items-center justify-center  w-80  bg-white rounded-full cursor-pointer  border-gray-400  hover:border-black  border-2  hover:bg-gray-100  text-black mx-2   py-3 ">
-        <img className=" w-6 mx-2 " src="./images/apple.png" alt="" />
-        <a href="https://www.instagram.com/abhinavdesigns1/">{props.button2}</a>
-      </div>
+      <a href="https://www.instagram.com/abhinavdesigns1/">
+        <button className=" flex  items-center justify-center  w-80  bg-white rounded-full cursor-pointer  border-gray-400  hover:border-black  border-2  hover:bg-gray-100  text-black mx-2   py-3 ">
+          <img className=" w-6 mx-2 " src="./images/apple.png" alt="" />
+          {props.button2}
+        </button>
+      </a>
     </div>
   );
 }
